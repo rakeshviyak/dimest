0d40747f822cbb6c73cebfdd8ad4bb4499353de9
Rakesh Viyak <rakeshviyak@gmail.com>
2013-11-04 02:05:30 +0800

    Added final template, Backbone.js support

--- hello.py
+++ hello.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+	return render_template('index.html', home=True)
+
+@app.route("/products")
+def products():
+    return render_template('backbone-products.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
